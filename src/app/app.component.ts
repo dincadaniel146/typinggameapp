7558eba407f68dd54893c1982d0d7481d3dcd50b
dincadaniel146 <dinca.daniel146@gmail.com>
2023-04-11 16:43:52 +0300

    proj commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typeapp';
+ randomlorem=faker.lorem.paragraphs(1)
+ enteredText=''
+ correctletters=0
+ incorrectletters=0
+ randomloremlength=this.randomlorem.length
+ typelength=this.randomloremlength
+ DetectUserInput(value:string)
+ {
+ this.enteredText=value;
+ }
+ compare(randomLetter:string,enteredLetter:string)
+ {
+  if(!enteredLetter){
+    return 'pending';
+  }
+  return randomLetter === enteredLetter ? 'correct' : 'incorrect'   //If randomLetter = enterLetter return correct else return incorrect.
+ } 
+ reload(){
+  window.location.reload();
+ }
 }
